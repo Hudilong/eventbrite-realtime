63fa1121bb253d32be6a4349dc1bc9b076bdb006
Ian Hudicourt <ihudicourt@gmail.com>
2021-11-01 13:18:09 -0400

    .

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
-      origin: "https://localhost:6313",
+      origin: "https://localhost:6613",
       methods: ["GET", "POST"],
       transports: ['websocket', 'polling'],
       credentials: true
