8dca1b4f6fcd0aefd91df745fe225e598b26f570
Ian Hudicourt <ihudicourt@gmail.com>
2021-11-01 01:35:54 -0400

    procfile added

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
-const io = require('socket.io')(server);
+const io = require('socket.io')(server, {
+  cors: {
+    origin: '*',
+  }
+});
 const PORT = process.env.PORT || 5000;
 
 
 io.on('connection', () => {
     console.log("A user has connected")
+});
+
+io.on('message', () => {
+  print(message);
 });
 
 app.get('/', (req, res) => {
