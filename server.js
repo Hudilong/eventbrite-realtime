a44223e1e6e385b10f32e562b12b896e84194ab4
Ian Hudicourt <ihudicourt@gmail.com>
2021-11-11 12:33:51 -0500

    test

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
@@ -13,21 +14,41 @@ const io = require('socket.io')(server, {
 const PORT = process.env.PORT || 5000;
 
 
+
 io.on('connection', (socket) => {
     console.log("A user has connected");
 
     //when server recieves a message
     socket.on('message', (data) => {
+
+      console.log(data.content);
+
+      //Send the message to clients
       socket.emit('message', data);
+
+      //Add Message to conversation
+
+      //Update lastMessage in conversation
     
     });
 
+    socket.on('newMessage', (data) => {
+      axios
+        .post('https://www.projetintegrateur.xyz/messengercontroller.php', data,{
+        headers: { "Content-Type": "multipart/form-data" }})
+        .then(res => {
+          console.log(`statusCode: ${res.status}`)
+          console.log(res)
+        })
+        .catch(error => {
+          console.error(error)
+        })
+      
+    });
 
     socket.on("connect_error", (err) => {
       console.log(`connect_error due to ${err.message}`);
     });
-
-
 });
 
 
